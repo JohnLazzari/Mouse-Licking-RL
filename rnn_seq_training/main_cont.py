b2e6282c38c07c81bf4cd1baff339be11d5989dc
JohnLazzari <lazzarijohn2@gmail.com>
2024-02-05 09:43:55 -0500

    organizing code

---
+++ rnn_seq_training/main_cont.py
@@ -0,0 +1,80 @@
+import gym
+import torch.optim as optim
+import numpy as np
+import scipy.io
+import matplotlib.pyplot as plt
+
+from sac_model import Actor, Critic, Actor_Seq
+from sac_learn import OptimizerSpec, sac_learn
+from utils.gym import get_env, get_wrapper_by_name
+from lick_env import Lick_Env_Cont, Trajectory_Env
+from utils.custom_optim import CustomAdamOptimizer
+import torch
+import config
+
+def main():
+
+    ### PARAMETERS ###
+    parser = config.config_parser()
+    args = parser.parse_args()
+
+    ### CREATE ENVIRONMENT ###
+    torch.manual_seed(args.seed)
+    
+    if args.env == "trajectory":
+        env = Trajectory_Env(args.action_dim, args.timesteps, args.dt, args.beta, args.bg_scale, args.alm_data_path)
+    elif args.env == "lick_ramp":
+        env = Lick_Env_Cont(args.action_dim, args.timesteps, args.thresh, args.dt, args.beta, args.bg_scale, args.alm_data_path, args.model)
+
+    ### RUN TRAINING ###
+    env = get_env(env, args.seed)
+
+    if args.model == "rnn":
+        agent = Actor
+        actor_optimizer_spec = OptimizerSpec(
+            constructor=optim.Adam,
+            kwargs=dict(lr=args.lr, weight_decay=args.weight_decay),
+        )
+        critic_optimizer_spec = OptimizerSpec(
+            constructor=optim.Adam,
+            kwargs=dict(lr=args.lr, weight_decay=args.weight_decay),
+        )
+    elif args.model == "rnn_seq":
+        agent = Actor_Seq
+        actor_optimizer_spec = OptimizerSpec(
+            constructor=CustomAdamOptimizer,
+            kwargs=dict(lr=args.lr),
+        )
+        critic_optimizer_spec = OptimizerSpec(
+            constructor=optim.Adam,
+            kwargs=dict(lr=args.lr),
+        )
+
+    sac_learn(
+        env,
+        args.seed,
+        args.inp_dim,
+        args.hidden_dim,
+        args.action_dim,
+        agent,
+        Critic,
+        actor_optimizer_spec,
+        critic_optimizer_spec,
+        args.policy_replay_size,
+        args.policy_batch_size,
+        args.alpha,
+        args.gamma,
+        args.automatic_entropy_tuning,
+        args.learning_starts,
+        args.learning_freq,
+        args.save_iter,
+        args.log_steps,
+        args.frame_skips,
+        args.model_save_path,
+        args.reward_save_path,
+        args.steps_save_path,
+        args.model
+    )
+
+if __name__ == '__main__':
+    main()
