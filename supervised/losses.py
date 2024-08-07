fixed bug in loss, only exc alm units trained to ramp

supervised/losses.py
@@ -24,7 +24,7 @@ def loss_d1d2(constraint_criterion,
     if type == "alm":
 
         loss = (
-                constraint_criterion(torch.mean(act[:, 500:, alm_start:alm_start+hid_dim], dim=-1, keepdim=True), neural_act_alm[:, 500:, :])
+                constraint_criterion(torch.mean(act[:, 500:, alm_start:alm_start + (hid_dim - int(0.3 * hid_dim))], dim=-1, keepdim=True), neural_act_alm[:, 500:, :])
                 + 1e-4 * torch.mean(torch.pow(act[:, 1000:, :], 2), dim=(1, 2, 0))  
                 )
     